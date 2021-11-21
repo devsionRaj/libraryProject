585b2fef6053fd85a8177a8e0198d9ee24784ef9
Raj Kumar Das <devsionraj@hotmail.com>
2021-11-21 09:03:06 +0530

    latest updates added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require('express-rate-limit');
 const mongoSanitize = require('express-mongo-sanitize');
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -13,10 +14,19 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Limit requests from same API(Please configure)
+const limiter = rateLimit({
+  max: 2,
+  windowMs: 60 * 60 * 1000,
+  message: `Too many requests from this IP, please try again in an hour!`,
+});
+
+app.use('/api', limiter);
+
 // Body parser, reading data from body into req.body
 app.use(express.json());
 
-app.use(mongoSanitize())
+app.use(mongoSanitize());
 
 // Routes
 app.use("/api/v1/users", userRoutes);
